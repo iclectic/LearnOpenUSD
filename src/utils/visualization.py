b6146369afb1fde499c5e8e151d5b7b4b412aa83
mati-nvidia <mcodesal@nvidia.com>
2025-09-29 17:50:00 -0400

    Change CDN for 3D viewport/usda libs

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -40,9 +40,9 @@ def _get_highlightjs_imports() -> str:
 
     """
     return """
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/python.min.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.9.0/styles/atom-one-dark.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="https://unpkg.com/@highlightjs/cdn-assets@11.9.0/highlight.min.js"></script>
+<script src="https://unpkg.com/@highlightjs/cdn-assets@11.9.0/languages/python.min.js"></script>
         """
 
 def _render_html_code_visualizer(
@@ -327,7 +327,7 @@ def DisplaySingleUSD(
 </style>
 
 <!-- Import the `<model-viewer>` component: -->
-<script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js"></script>
+<script type="module" src="https://unpkg.com/@google/model-viewer@3.5.0/dist/model-viewer.min.js"></script>
 
 <!-- Import Bootstrap for layout: -->
 <!--
@@ -583,7 +583,7 @@ def DisplayUSD(
 </style>
 
 <!-- Import the `<model-viewer>` component: -->
-<script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js"></script>
+<script type="module" src="https://unpkg.com/@google/model-viewer@3.5.0/dist/model-viewer.min.js"></script>
 
 <!-- Import Bootstrap for layout: -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
